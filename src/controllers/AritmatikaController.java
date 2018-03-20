aeed4783d151fe86d889e0d5a9404aa37047be6a
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2018-03-20 21:18:19 +0700

    integrasi

--- src/controllers/AritmatikaController.java
+++ src/controllers/AritmatikaController.java
@@ -5,7 +5,10 @@
  */
 package controllers;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import models.Aritmatika;
+import view.Kalkulator;
 
 /**
  *
@@ -13,5 +16,24 @@ import models.Aritmatika;
  */
 public class AritmatikaController {
     
+    private Kalkulator view;
+    private Aritmatika model;
+
+    public AritmatikaController(Kalkulator view, Aritmatika model) {
+        this.view = view;
+        this.model = model;
+        this.view.addTambahListener(new TambahListener());
+    }
+    
+    class TambahListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            model.setBil1(view.getBil1());
+            model.setBil2(view.getBil2());
+            view.setHasil(model.getPenjumlahan());
+        }
+        
+    }
     
 }
